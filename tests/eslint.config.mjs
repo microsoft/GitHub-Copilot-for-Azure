1337e0b3cf51e7f415dac525a2c43796a99f52af
Chunan Ye <chuye@microsoft.com>
2026-02-19 13:45:34 -0800

    Add eslint to scripts project

--- tests/eslint.config.mjs
+++ tests/eslint.config.mjs
@@ -1,4 +1,5 @@
 import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 import jest from "eslint-plugin-jest";
 import integrationTestNameRule from "./eslint-rules/integration-test-name.mjs";
@@ -37,7 +38,7 @@ const sharedRules = {
     "quotes": ["error", "double", { "avoidEscape": true }],
 };
 
-export default tseslint.config(
+export default defineConfig(
     // Global ignores
     {
         ignores: [
