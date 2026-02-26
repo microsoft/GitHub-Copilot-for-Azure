749cffd5a73bfbd3329f263f4ecb67cf8fb625e9
Alex Thompson <alexthompson@microsoft.com>
2026-02-26 17:18:45 -0500

    Added missing underscore to integration test name

--- tests/azure-compute/integration.test.ts
+++ tests/azure-compute/integration.test.ts
@@ -31,7 +31,7 @@ if (skipTests && skipReason) {
 
 const describeIntegration = skipTests ? describe.skip : describe;
 
-describeIntegration(`${SKILL_NAME} - Integration Tests`, () => {
+describeIntegration(`${SKILL_NAME}_ - Integration Tests`, () => {
     const agent = useAgentRunner();
 
     describe("skill-invocation", () => {
