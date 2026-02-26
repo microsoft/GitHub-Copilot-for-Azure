142a42dd18ad9de9a444c447fb5964b7d0c6f693
Patrick Hallisey <pahallis@microsoft.com>
2026-02-26 13:29:31 -0800

    Use updated MI name

--- pipelines/scripts/Invoke-CopilotBenchmarks.ps1
+++ pipelines/scripts/Invoke-CopilotBenchmarks.ps1
@@ -2,5 +2,11 @@ param(
     [string]$BuildId
 )
 
+
+
+# Install MSBench CLI
+Write-Host "Installing keyring"
+pip install keyring artifacts-keyring
+
 Write-Host "Listing key vaults in the resource group"
 az keyvault list --resource-group rg-msbench-eval-kv-azure-mcp --query "[].name" -o tsv
