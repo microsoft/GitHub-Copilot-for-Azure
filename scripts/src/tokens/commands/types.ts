566be803bfc5545099ec7122d0facb4b36c38258
Chunan Ye <chuye@microsoft.com>
2026-03-02 13:39:07 -0800

    Typecheck typescript code in scripts

--- scripts/src/tokens/commands/types.ts
+++ scripts/src/tokens/commands/types.ts
@@ -167,6 +167,7 @@ export function matchesPattern(filePath: string, pattern: string): boolean {
     return normalizedPath.endsWith("/" + pattern) || normalizedPath === pattern;
   }
 
+  // @ts-ignore Using named import satisfies Typescript's static analysis but the code won't work at runtime. Using default import works at runtime but fails the static analysis.
   return isMatch(normalizedPath, pattern);
 }
 
