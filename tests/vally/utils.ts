d61d23499e93d45db212aeff74136a8874c137c1
Chunan Ye <chuye@microsoft.com>
2026-05-29 11:03:30 -0700

    Migrate appinsights-instrumentation, azure-cost, azure-messaging, azure-quotas Fix issues in migrated eval suites

--- tests/vally/utils.ts
+++ tests/vally/utils.ts
@@ -1,5 +1,7 @@
 export function normalizeTestName(skillName: string, testName: string) {
-  testName = testName.replace(/\s+/g, "_");
+  // Downstream data processing use the test name as Azure Storage blob names
+  // We need to replace unsupported patterns withs supported patterns. 
+  testName = testName.replace(/\s+/g, "_").replace(/[:<>|*?]/g, "_");
   if (!testName.startsWith(`${skillName}_`)) {
     testName = `${skillName}_${testName}`;
   }
